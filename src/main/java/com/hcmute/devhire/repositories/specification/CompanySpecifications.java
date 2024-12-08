008b39bbc79c4119b052600289990ca0579a7979
havu88 <hoanghavuvta09@gmail.com>
2024-12-08 21:27:44 +0700

    fix: search Company (add address)

--- src/main/java/com/hcmute/devhire/repositories/specification/CompanySpecifications.java
+++ src/main/java/com/hcmute/devhire/repositories/specification/CompanySpecifications.java
@@ -5,6 +5,11 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class CompanySpecifications {
     public static Specification<Company> hasKeyword(String keyword) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get("name"), "%" + keyword + "%");
+        return (root, query, criteriaBuilder) -> {
+            return criteriaBuilder.or(
+                    criteriaBuilder.like(root.get("name"), "%" + keyword + "%"),
+                    criteriaBuilder.like(root.get("address"), "%" + keyword + "%")
+            );
+        };
     }
 }
