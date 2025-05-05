1f184b2d420507cf8a4d40725090ea1be8e5a482
ThuatVDQ <thuatvopro@gmail.com>
2025-05-05 11:21:41 +0700

    fix: use jwt for recommendation

--- src/main/java/com/hcmute/devhire/controllers/JobRecommendationController.java
+++ src/main/java/com/hcmute/devhire/controllers/JobRecommendationController.java
@@ -1,5 +1,6 @@
 package com.hcmute.devhire.controllers;
 import com.hcmute.devhire.DTOs.JobDTO;
+import com.hcmute.devhire.components.JwtUtil;
 import com.hcmute.devhire.entities.Job;
 import com.hcmute.devhire.services.IRecommendationService;
 import lombok.RequiredArgsConstructor;
@@ -14,14 +15,14 @@ public class JobRecommendationController {
     private final IRecommendationService recommendationService;
 
     @GetMapping("")
-    public ResponseEntity<List<JobDTO>> getRecommendedJobs(
-            @RequestParam String email,
+    public ResponseEntity<?> getRecommendedJobs(
             @RequestParam(defaultValue = "5") int topK) {
         try {
-            List<JobDTO> jobs = recommendationService.recommendJobsForUser(email, topK);
+            String username = JwtUtil.getAuthenticatedUsername();
+            List<JobDTO> jobs = recommendationService.recommendJobsForUser(username, topK);
             return ResponseEntity.ok(jobs);
         } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(null);
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 }
