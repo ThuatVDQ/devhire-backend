f61bf1429e96ec7a87709691a41e75eba2ac987d
ThuatVDQ <thuatvopro@gmail.com>
2025-06-17 15:33:30 +0700

    fix: get all cv of user

--- src/main/java/com/hcmute/devhire/services/CVService.java
+++ src/main/java/com/hcmute/devhire/services/CVService.java
@@ -16,6 +16,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -48,7 +49,9 @@ public class CVService implements ICVService{
 
     @Override
     public List<CV> findByUserId(Long userId) {
-        return cvRepository.findByUserId(userId);
+        List<CV> cvs = cvRepository.findByUserId(userId);
+        cvs.sort(Comparator.comparing(CV::getUpdatedAt).reversed());
+        return cvs;
     }
 
     @Override
